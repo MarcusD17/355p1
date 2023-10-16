3a77bc4c6e57535a9eaad05db7ebd315c56ddfcc
MarcusD17 <60440487+MarcusD17@users.noreply.github.com>
2023-10-15 20:06:42 -0400

    Committing batch 2
    Adding more files from local repository

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project1.Models;
+using System.Diagnostics;
+
+namespace Project1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Language()
+        {
+            return View();
+        }
+        public IActionResult CareersAuth()
+        {
+            return View();
+        }
+        public IActionResult Careers()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
